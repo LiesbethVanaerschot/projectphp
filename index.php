d61ec6db7f8d5b45aff01e94d987e4b95fba091b
ZiggyV <ziggy.verstrepen@gmail.com>
2014-04-15 17:55:56 +0200

    layout personeel en index added

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
-<?php
-	// feedback en session een default waarde geven
-	$feedbackEr = "";
-	$_SESSION['loggedin'] = false;
-
-	// Zien of post niet leeg is en user uit databank halen
-	if(!empty($_POST))
-	{
-		try
-		{
-			include_once("classes/User.class.php");
-
-			$u = new User();
-			$u->Email = $_POST['email'];
-			$u->Password = $_POST['password'];
-			$u->Find();
-
-		} catch (Exception $e) {
-			$feedbackEr = $e->getMessage();
-		}
-	}
-?><!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -31,91 +10,18 @@
 </head>
 <body>
 	<div id="container">
-		<nav <?php 	if($_SESSION['loggedin'] == false){
-						echo 'class="hidden"';
-				  	}
-				  	else
-				  	{
-				  		echo 'class="block"';
-				  	}
-			  ?>> <!-- nav verschijnt als je inlogt -->
-			<a href="logout.php">LOG OUT</a>
-		</nav> <!-- End nav -->
-
 		<header>
 			<h1>MoreSchedule</h1>
 		</header> <!--  End header -->
 
-		<section id="login" <?php if($_SESSION['loggedin'] == true){
-										echo 'class="hidden"';
-								  }
-							?>> <!-- als je inlogt dan moet de loginform verdwijnen -->
+		<section id="index">
 			<div class="wrapper">
-				<h1 class="gray">Login</h1>
-				<h2>Importeer de tblusers</h2>
-				<h2>email=r0417768 of r0382075 && pw=test123</h2>
-				<form action="" method="post">
-					<p class="cf">
-						<label for="name">Studentennummer</label>
-						<input type="text" name="email" id="email" placeholder="r0382075">
-					</p>
-					<p class="cf">
-						<label for="password">Paswoord</label>
-						<input type="password" name="password" id="password" placeholder="paswoord" >
-					</p>
-					<p class="cf">
-						<input type="submit" name="btnLogin" value="LOG IN">
-					</p>
-				</form>
-
-				<div id="feedback">
-					<p class="nok"><?php echo $feedbackEr; ?></p>
+				<div class="redirect">
+					<a href="student.php">Log in als student</a>
+					<a href="personeel.php">Log in als personeel</a>
 				</div>
 			</div>
-		</section><!-- End login -->
-
-		<section id="loggedin" <?php if($_SESSION['loggedin'] == false){
-											echo 'class="hidden"';
-									 }
-									 else{
-									 		echo 'class="block"';
-									 }
-							   ?>>
-			<div class="wrapper">
-				<h1>Kies uw klas!</h1>
-
-				<div class="klas1">
-					<p>1IMDa</p>
-					<p>1IMDb</p>
-					<p>1IMDc</p>
-				</div>
-
-				<table> <!-- hier alle klassen geven met php eventueel? zodat we niet altijd moete copy pasten -->
-					<tr>
-						<td><a href="#">1IMDa</a></td>
-					</tr>
-					<tr>
-						<td><a href="#">1IMDb</a></td>
-					</tr>
-					<tr>
-						<td><a href="#">1IMDc</a></td>
-					</tr>
-					<tr>
-						<td><a href="#">2IMDa</a></td>
-					</tr>
-					<tr>
-						<td><a href="#">2IMDb</a></td>
-					</tr>
-					<tr>
-						<td><a href="#">3IMDa</a></td>
-					</tr>
-				</table>
-			</div>
-		</section><!-- End loggedin -->
-	</div> <!-- End container -->
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-
-	<!-- ./JS -->
+		</section>
+	</div>
 </body>
 </html>
