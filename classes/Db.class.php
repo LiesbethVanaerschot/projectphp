219f630d573d18c6ff8efa15ec41a3507ca45e9d
SaarPelgrims <sarah.pelgrims@student.thomasmore.be>
2014-04-17 13:37:13 +0200

    jijwilteenwebsi db added

--- classes/Db.class.php
+++ classes/Db.class.php
@@ -4,13 +4,8 @@
 	{
 		private $m_sHost = "localhost";
 		private $m_sUser = "root";
-<<<<<<< HEAD
-		private $m_sPassword = "root";
-		private $m_sDatabase = "project";
-=======
 		private $m_sPassword = "";
 		private $m_sDatabase = "jijwilteenwebsi";
->>>>>>> 53dc2b9c66ea2d9be9073fe29132d559e50a8f63
 		public $conn;
 
 		public function __construct()
