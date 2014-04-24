019bee17ef860efc4f94edf8ea8f58c0c061ac9d
SaarPelgrims <sarah.pelgrims@student.thomasmore.be>
2014-04-24 16:58:43 +0200

    feedback HENS in commentaar

--- ajax/sendingvar.php
+++ ajax/sendingvar.php
@@ -1,6 +1,7 @@
 <?php 
-
 	$dag = $_POST['dag'];
 	echo $dag;
-
+	// hier query met dag die je hebt doorgegeven met ajax
+	// dan terugsturen met json_encode
+	// dan in student.php uitloopen met jquery
 ?>
--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         var weekdag = $(this).text();
         //meegeven aan pagina
         window.location.href = "weekdag.php?dag=" + weekdag;
+
+        /* WEEKDAG.PHP BESTAAT NIET MEER, DEZE PAGINA WAS OVERB*/
     });
 
     var dagArray = ["maandag","dinsdag","woensdag","donderdag","vrijdag"];
@@ -22,8 +24,37 @@ $(document).ready(function(){
     	
     	//$.get("student.php?dag=" + dag);
 
+      // var dag = $("#dag").html();
+      // dag niet uit een url halen maar gwn uit de array of veld
+      // $("#next").on('click', function(){
+      //     var dag = $("#dag").html();
+
+      //     var request = $.ajax({
+      //             url: "../ajax/sendingvar.php",
+                  
+      //             HIER MOET JE NAAR VOLGENDE LINKEN   url: "./ajax/sendingvar.php"      zodat uwe student.php het niet allemaal moet laden.
+
+      //             In sendingvar.php, hierin moet je een query maken van uwen dag dat je hebt doorgestuurd.
+      //             Het resultaat met je json_encode terugsturen naar student.php en daar moet je dan een loop nodig hebben om in tabel weertegeven (zoals api).
+                  
+      //             type: "POST",
+      //             data: {'dag' : dag},
+      //             dataType: "html"
+      //       });
+      //       request.done(function(msg){
+      //         console.log("gestuurd!");
+      //       })
+      //     });
+      // });
+
    		var request = $.ajax({
    			url: "./student.php",
+        /*
+        HIER MOET JE NAAR VOLGENDE LINKEN   url: "./ajax/sendingvar.php"      zodat uwe student.php het niet allemaal moet laden.
+
+        In sendingvar.php, hierin moet je een query maken van uwen dag dat je hebt doorgestuurd.
+        Het resultaat met je json_encode terugsturen naar student.php en daar moet je dan een loop nodig hebben om in tabel weertegeven (zoals api).
+        */
    			type: "POST",
    			data: {'dag' : dag},
    			dataType: "html"
