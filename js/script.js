8465d654e67aa901cb71ab0efc22413da8508cbf
Lissa Sleeckx <info@lissasleeckx.be>
2014-05-09 12:14:26 +0200

    Fix vakken met meerdere lesblokken

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     var dagArray = ["maandag","dinsdag","woensdag","donderdag","vrijdag"];
-    console.log(dagArray);
+    // console.log(dagArray);
     $("#dag").text(dagArray[0]);
 
     var i = 0;
-    console.log(dagArray[i]);
+    // console.log(dagArray[i]);
 
 //FUNCTIES
 //reset functie uitschrijven
@@ -16,10 +16,10 @@ $(document).ready(function(){
 //getschedule functie
   var getschedule = function(){
     var dag = $("#dag").html();
-    console.log(dag);
+    // console.log(dag);
         
     var student = $(".user").html();
-    console.log(student);
+    // console.log(student);
 
     var request = $.ajax({
           url: "./ajax/sendingvar.php",
@@ -28,105 +28,76 @@ $(document).ready(function(){
           dataType: "html"
         });
         request.done(function(msg){
-          console.log("gestuurd!");
+          // console.log("gestuurd!");
           //var json = msg;
           var json = jQuery.parseJSON(msg);
-          console.log(json);
-          //iets met als json === "" alle text in tabel weg 
+          // console.log(json);
+          // iets met als json === "" alle text in tabel weg 
           // als json !== "" $.each en dan met switch case tabel invullen.
             if(json.length){
               $.each(json, function(index, value){
-                console.log(index);
-                console.log(value);
+                // console.log(index);
+                // console.log(value.lesBegin);
+                // console.log(value);
+                var lesBegin = value.lesBegin;
+                var lesEinde = value.lesEind;
+                var beginString = lesBegin.split(':');
+                var eindeString = lesEinde.split(':');
+                var beginUur = new Date('2014', '05', '07', beginString[0], beginString[1]);
+                var eindUur = new Date('2014', '05', '07', eindeString[0], eindeString[1]);
+
                 var begin = $('.td-beginuur').html();
                 var eind = $('.td-einduur').html();
 
-                switch(value.lesBegin){
-                  case '08:30':
-                      $('.l1').html(value.lesNaam);
-                      $('.lo1').html(value.lesLokaal);
-                      $('.d1').html(value.docentNaam);
-                    break;
-                  case '09:30':
-                    $('.l2').html(value.lesNaam);
-                    $('.lo2').html(value.lesLokaal);
-                    $('.d2').html(value.docentNaam);
-                    break; 
-                  case '10:45':
-                    $('.l4').html(value.lesNaam);
-                    $('.lo4').html(value.lesLokaal);
-                    $('.d4').html(value.docentNaam);
-                    break;
-                  case '11:45':
-                    $('.l5').html(value.lesNaam);
-                    $('.lo5').html(value.lesLokaal);
-                    $('.d5').html(value.docentNaam);
-                    break;
-                  case '13:45':
-                    $('.l7').html(value.lesNaam);
-                    $('.lo7').html(value.lesLokaal);
-                    $('.d7').html(value.docentNaam);
-                    break; 
-                  case '14:45':
-                    $('.l8').html(value.lesNaam);
-                    $('.lo8').html(value.lesLokaal);
-                    $('.d8').html(value.docentNaam);
-                    break;
-                  case '16:00':
-                    $('.l10').html(value.lesNaam);
-                    $('.lo10').html(value.lesLokaal);
-                    $('.d10').html(value.docentNaam);
-                    break; 
-                  case '17:00':
-                    $('.l11').html(value.lesNaam);
-                    $('.lo11').html(value.lesLokaal);
-                    $('.d11').html(value.docentNaam);
-                    break;            
+                // console.log(value.lesNaam);
+                // console.log(new Date('2014', '05', '07', '08', '30').getTime());
+                // console.log(beginUur.getTime());
+                // console.log(new Date('2014', '05', '07', '09', '30').getTime());
+                // console.log('einduur ' + eindUur.getTime());
+
+                //OPLOSSING DIE ERVOOR ZORGT DAT VAKKEN ZOALS PROJECT MET MEERDERE LESBLOKKEN OP ELK LESBLOK WORDT GETOOND
+                if(new Date('2014', '05', '07', '08', '30').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '08', '30').getTime() < eindUur.getTime()) {
+                  $('.l1').html(value.lesNaam);
+                  $('.lo1').html(value.lesLokaal);
+                  $('.d1').html(value.docentNaam);
+                }
+                // NIETINGIT
+                if(new Date('2014', '05', '07', '09', '30').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '09', '30').getTime() < eindUur.getTime()) {
+                  $('.l2').html(value.lesNaam);
+                  $('.lo2').html(value.lesLokaal);
+                  $('.d2').html(value.docentNaam);
+                }
+                if(new Date('2014', '05', '07', '10', '45').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '10', '45').getTime() < eindUur.getTime()) {
+                  $('.l4').html(value.lesNaam);
+                  $('.lo4').html(value.lesLokaal);
+                  $('.d4').html(value.docentNaam);
+                }
+                if(new Date('2014', '05', '07', '11', '45').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '11', '45').getTime() < eindUur.getTime()) {
+                  $('.l5').html(value.lesNaam);
+                  $('.lo5').html(value.lesLokaal);
+                  $('.d5').html(value.docentNaam);
+                }
+                if(new Date('2014', '05', '07', '13', '45').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '13', '45').getTime() < eindUur.getTime()) {
+                  $('.l7').html(value.lesNaam);
+                  $('.lo7').html(value.lesLokaal);
+                  $('.d7').html(value.docentNaam);
+                }
+                if(new Date('2014', '05', '07', '14', '45').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '14', '45').getTime() < eindUur.getTime()) {
+                  $('.l8').html(value.lesNaam);
+                  $('.lo8').html(value.lesLokaal);
+                  $('.d8').html(value.docentNaam);
+                }
+                if(new Date('2014', '05', '07', '16', '00').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '16', '00').getTime() < eindUur.getTime()) {
+                  $('.l10').html(value.lesNaam);
+                  $('.lo10').html(value.lesLokaal);
+                  $('.d10').html(value.docentNaam);
+                }
+                if(new Date('2014', '05', '07', '17', '00').getTime() >= beginUur.getTime() && new Date('2014', '05', '07', '17', '00').getTime() < eindUur.getTime()) {
+                  $('.l11').html(value.lesNaam);
+                  $('.lo11').html(value.lesLokaal);
+                  $('.d11').html(value.docentNaam);
                 }
 
-                switch(value.lesEind){
-                  case '09:30':
-                      $('.l1').html(value.lesNaam);
-                      $('.lo1').html(value.lesLokaal);
-                      $('.d1').html(value.docentNaam);
-                    break;
-                  case '10:30':
-                      $('.l2').html(value.lesNaam);
-                      $('.lo2').html(value.lesLokaal);
-                      $('.d2').html(value.docentNaam);
-                    break; 
-                  case '11:45':
-                      $('.l4').html(value.lesNaam);
-                      $('.lo4').html(value.lesLokaal);
-                      $('.d4').html(value.docentNaam);
-                    break;
-                  case '12:45':
-                      $('.l5').html(value.lesNaam);
-                      $('.lo5').html(value.lesLokaal);
-                      $('.d5').html(value.docentNaam);
-                    break;
-                  case '14:45':
-                      $('.l7').html(value.lesNaam);
-                      $('.lo7').html(value.lesLokaal);
-                      $('.d7').html(value.docentNaam);
-                    break; 
-                  case '15:45':
-                      $('.l8').html(value.lesNaam);
-                      $('.lo8').html(value.lesLokaal);
-                      $('.d8').html(value.docentNaam);
-                    break;
-                  case '17:00':
-                      $('.l10').html(value.lesNaam);
-                      $('.lo10').html(value.lesLokaal);
-                      $('.d10').html(value.docentNaam);
-                    break; 
-                  case '18:00':
-                      $('.l11').html(value.lesNaam);
-                      $('.lo11').html(value.lesLokaal);
-                      $('.d11').html(value.docentNaam);
-                    break;            
-                }
-                   console.log(begin);
 
               });
             }
