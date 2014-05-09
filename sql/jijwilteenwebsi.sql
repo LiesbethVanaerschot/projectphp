05ed66c092fa7fd3fc786b99959298c4e6cc8995
LiesbethVanaerschot <lies.vanaerschot@gmail.com>
2014-05-09 13:17:08 +0200

    nieuwe db

--- sql/jijwilteenwebsi.sql
+++ sql/jijwilteenwebsi.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9.2
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
--- Machine: localhost:8889
--- Genereertijd: 07 Mei 2014 om 21:54
--- Serverversie: 5.5.9
--- PHP-Versie: 5.3.5
+-- Machine: 127.0.0.1
+-- Gegenereerd op: 09 mei 2014 om 13:15
+-- Serverversie: 5.6.16
+-- PHP-versie: 5.5.9
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,16 +17,16 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `jijwilteenwebsi`
+-- Databank: `jijwilteenwebsi`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblAdmin`
+-- Tabelstructuur voor tabel `tbladmin`
 --
 
-CREATE TABLE `tblAdmin` (
+CREATE TABLE IF NOT EXISTS `tbladmin` (
   `adminID` int(255) NOT NULL AUTO_INCREMENT,
   `adminUnummer` varchar(11) NOT NULL,
   `adminPaswoord` varchar(11) NOT NULL,
@@ -33,18 +34,19 @@ CREATE TABLE `tblAdmin` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Gegevens worden uitgevoerd voor tabel `tblAdmin`
+-- Gegevens worden uitgevoerd voor tabel `tbladmin`
 --
 
-INSERT INTO `tblAdmin` VALUES(1, 'u0123456', 'test123');
+INSERT INTO `tbladmin` (`adminID`, `adminUnummer`, `adminPaswoord`) VALUES
+(1, 'u0123456', 'test123');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblDocent`
+-- Tabelstructuur voor tabel `tbldocent`
 --
 
-CREATE TABLE `tblDocent` (
+CREATE TABLE IF NOT EXISTS `tbldocent` (
   `docentID` int(11) NOT NULL AUTO_INCREMENT,
   `docentNaam` varchar(255) NOT NULL,
   `lesID` int(11) NOT NULL,
@@ -52,36 +54,37 @@ CREATE TABLE `tblDocent` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 --
--- Gegevens worden uitgevoerd voor tabel `tblDocent`
+-- Gegevens worden uitgevoerd voor tabel `tbldocent`
 --
 
-INSERT INTO `tblDocent` VALUES(1, 'Moonen N.', 1);
-INSERT INTO `tblDocent` VALUES(2, 'Lenaerts M.', 2);
-INSERT INTO `tblDocent` VALUES(3, 'Hens J.', 4);
-INSERT INTO `tblDocent` VALUES(4, 'Aerts K.', 5);
-INSERT INTO `tblDocent` VALUES(5, 'Rutten K.', 6);
-INSERT INTO `tblDocent` VALUES(6, 'Vanelderen R.', 7);
-INSERT INTO `tblDocent` VALUES(7, 'Geussens P.', 8);
-INSERT INTO `tblDocent` VALUES(8, 'Heerinckx D.', 11);
-INSERT INTO `tblDocent` VALUES(9, 'Moonen N.', 3);
-INSERT INTO `tblDocent` VALUES(10, 'Lenaerts M.', 14);
-INSERT INTO `tblDocent` VALUES(11, 'Lenaerts M.', 15);
-INSERT INTO `tblDocent` VALUES(12, 'Hens J.', 10);
-INSERT INTO `tblDocent` VALUES(13, 'Hens J.', 13);
-INSERT INTO `tblDocent` VALUES(14, 'Hens J.', 12);
-INSERT INTO `tblDocent` VALUES(15, 'Aerts K.', 9);
-INSERT INTO `tblDocent` VALUES(16, 'Rutten K.', 9);
-INSERT INTO `tblDocent` VALUES(17, 'Vanelderen R.', 9);
-INSERT INTO `tblDocent` VALUES(18, 'Geussens P.', 16);
-INSERT INTO `tblDocent` VALUES(19, 'Geussens P.', 17);
+INSERT INTO `tbldocent` (`docentID`, `docentNaam`, `lesID`) VALUES
+(1, 'Moonen N.', 1),
+(2, 'Lenaerts M.', 2),
+(3, 'Hens J.', 4),
+(4, 'Aerts K.', 5),
+(5, 'Rutten K.', 6),
+(6, 'Vanelderen R.', 7),
+(7, 'Geussens P.', 8),
+(8, 'Heerinckx D.', 11),
+(9, 'Moonen N.', 3),
+(10, 'Lenaerts M.', 14),
+(11, 'Lenaerts M.', 15),
+(12, 'Hens J.', 10),
+(13, 'Hens J.', 13),
+(14, 'Hens J.', 12),
+(15, 'Aerts K.', 9),
+(16, 'Rutten K.', 9),
+(17, 'Vanelderen R.', 9),
+(18, 'Geussens P.', 16),
+(19, 'Geussens P.', 17);
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblLes`
+-- Tabelstructuur voor tabel `tblles`
 --
 
-CREATE TABLE `tblLes` (
+CREATE TABLE IF NOT EXISTS `tblles` (
   `lesID` int(11) NOT NULL AUTO_INCREMENT,
   `lesNaam` varchar(255) NOT NULL,
   `lesDag` varchar(255) NOT NULL,
@@ -93,57 +96,57 @@ CREATE TABLE `tblLes` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
 
 --
--- Gegevens worden uitgevoerd voor tabel `tblLes`
+-- Gegevens worden uitgevoerd voor tabel `tblles`
 --
 
-INSERT INTO `tblLes` VALUES(1, 'Engels 2', 'maandag', '09:30', '11:45', 'K3/5', 2);
-INSERT INTO `tblLes` VALUES(2, 'Methoden van onderzoek en rapportering - theorie', 'maandag', '11:45', '12:45', 'T014', 2);
-INSERT INTO `tblLes` VALUES(3, 'Engels 2', 'maandag', '13:45', '15:45', 'K3/5', 2);
-INSERT INTO `tblLes` VALUES(4, 'PHP1', 'dinsdag', '08:30', '10:30', 'K3/5', 2);
-INSERT INTO `tblLes` VALUES(5, 'Projectmanagement', 'dinsdag', '10:45', '12:45', 'T014', 2);
-INSERT INTO `tblLes` VALUES(6, 'Digital Publishing', 'dinsdag', '13:45', '18:00', 'K0/3', 2);
-INSERT INTO `tblLes` VALUES(7, 'Content Management Systemen 2', 'dinsdag', '13:45', '17:00', 'K3/3', 2);
-INSERT INTO `tblLes` VALUES(8, 'Ethiek 1 - theorie', 'woensdag', '08:30', '09:30', 'T014', 2);
-INSERT INTO `tblLes` VALUES(9, 'Project 2', 'woensdag', '09:30', '18:00', 'Creativity Gym', 2);
-INSERT INTO `tblLes` VALUES(10, 'PHP1', 'donderdag', '14:45', '18:00', 'K3/5', 2);
-INSERT INTO `tblLes` VALUES(11, 'Motion Design', 'vrijdag', '08:30', '10:30', 'K3/3', 2);
-INSERT INTO `tblLes` VALUES(12, 'Webtechnologie 2', 'vrijdag', '08:30', '10:30', 'Creativity Gym', 2);
-INSERT INTO `tblLes` VALUES(13, 'PHP1', 'vrijdag', '11:45', '12:45', 'K3/1', 2);
-INSERT INTO `tblLes` VALUES(14, 'Methoden van onderzoek en rapportering - practicum', 'vrijdag', '12:45', '13:45', 'K3/1', 2);
-INSERT INTO `tblLes` VALUES(15, 'Methoden van onderzoek en rapportering - practicum', 'vrijdag', '13:45', '14:45', 'K3/1', 2);
-INSERT INTO `tblLes` VALUES(16, 'Ethiek 1 - Begeleiding', 'vrijdag', '13:45', '14:45', 'K1/1', 2);
-INSERT INTO `tblLes` VALUES(17, 'Ethiek 1 - Begeleiding', 'vrijdag', '14:45', '15:45', 'K1/1', 2);
-INSERT INTO `tblLes` VALUES(24, 'Webtechnologie 2', 'vrijdag', '10:45', '11:45', 'Creativity Gym', 2);
-INSERT INTO `tblLes` VALUES(23, 'Digital Publishing', 'dinsdag', '16:00', '18:00', 'K0/3', 3);
+INSERT INTO `tblles` (`lesID`, `lesNaam`, `lesDag`, `lesBegin`, `lesEind`, `lesLokaal`, `lesSemester`) VALUES
+(1, 'Engels 2', 'maandag', '09:30', '11:45', 'K3/5', 2),
+(2, 'Methoden van onderzoek en rapportering - theorie', 'maandag', '11:45', '12:45', 'T014', 2),
+(3, 'Engels 2', 'maandag', '13:45', '15:45', 'K3/5', 2),
+(4, 'PHP1', 'dinsdag', '08:30', '10:30', 'K3/5', 2),
+(5, 'Projectmanagement', 'dinsdag', '10:45', '12:45', 'T014', 2),
+(6, 'Digital Publishing', 'dinsdag', '13:45', '18:00', 'K0/3', 2),
+(7, 'Content Management Systemen 2', 'dinsdag', '13:45', '17:00', 'K3/3', 2),
+(8, 'Ethiek 1 - theorie', 'woensdag', '08:30', '09:30', 'T014', 2),
+(9, 'Project 2', 'woensdag', '09:30', '18:00', 'Creativity Gym', 2),
+(10, 'PHP1', 'donderdag', '14:45', '18:00', 'K3/5', 2),
+(11, 'Motion Design', 'vrijdag', '08:30', '10:30', 'K3/3', 2),
+(12, 'Webtechnologie 2', 'vrijdag', '08:30', '11:45', 'Creativity Gym', 2),
+(13, 'PHP1', 'vrijdag', '11:45', '12:45', 'K3/1', 2),
+(14, 'Methoden van onderzoek en rapportering - practicum', 'vrijdag', '12:45', '13:45', 'K3/1', 2),
+(15, 'Methoden van onderzoek en rapportering - practicum', 'vrijdag', '13:45', '14:45', 'K3/1', 2),
+(16, 'Ethiek 1 - Begeleiding', 'vrijdag', '13:45', '14:45', 'K1/1', 2),
+(17, 'Ethiek 1 - Begeleiding', 'vrijdag', '14:45', '15:45', 'K1/1', 2);
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblMelding`
+-- Tabelstructuur voor tabel `tblmelding`
 --
 
-CREATE TABLE `tblMelding` (
+CREATE TABLE IF NOT EXISTS `tblmelding` (
   `meldingID` int(255) NOT NULL AUTO_INCREMENT,
   `melding` text NOT NULL,
   PRIMARY KEY (`meldingID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Gegevens worden uitgevoerd voor tabel `tblMelding`
+-- Gegevens worden uitgevoerd voor tabel `tblmelding`
 --
 
-INSERT INTO `tblMelding` VALUES(1, '6/05/2014 (Dinsdag) - Digital Publishing (Rutten K.) : blabla.');
-INSERT INTO `tblMelding` VALUES(2, '9/05/2014 (Vrijdag) - Hens J. (Webtechnologie 2) zal niet aanwezig zijn');
-INSERT INTO `tblMelding` VALUES(3, '9/05/2014 (Vrijdag) - Geussens P. (Ethiek 1 - Begeleiding) zal niet aanwezig zijn');
-INSERT INTO `tblMelding` VALUES(4, '16/05/2014 (Vrijdag) - Methoden van onderzoek en rapportering - practicum (Lenaerts M.) zal uitzonderlijk doorgaan in lokaal gym.');
+INSERT INTO `tblmelding` (`meldingID`, `melding`) VALUES
+(1, '6/05/2014 (Dinsdag) - Digital Publishing (Rutten K.) : blabla.'),
+(2, '9/05/2014 (Vrijdag) - Hens J. (Webtechnologie 2) zal niet aanwezig zijn'),
+(3, '9/05/2014 (Vrijdag) - Geussens P. (Ethiek 1 - Begeleiding) zal niet aanwezig zijn'),
+(4, '16/05/2014 (Vrijdag) - Methoden van onderzoek en rapportering - practicum (Lenaerts M.) zal uitzonderlijk doorgaan in lokaal gym.');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblStudent`
+-- Tabelstructuur voor tabel `tblstudent`
 --
 
-CREATE TABLE `tblStudent` (
+CREATE TABLE IF NOT EXISTS `tblstudent` (
   `studentID` int(255) NOT NULL AUTO_INCREMENT,
   `studentRnummer` varchar(8) NOT NULL,
   `studentPaswoord` varchar(255) NOT NULL,
@@ -154,21 +157,22 @@ CREATE TABLE `tblStudent` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Gegevens worden uitgevoerd voor tabel `tblStudent`
+-- Gegevens worden uitgevoerd voor tabel `tblstudent`
 --
 
-INSERT INTO `tblStudent` VALUES(1, 'r0330949', 'test1', 'r0330949@student.thomasmore.be', 'Liesbeth', 'Vanaerschot');
-INSERT INTO `tblStudent` VALUES(2, 'r0376986', 'test2', 'r0376986@student.thomasmore.be', 'Lissa', 'Sleeckx');
-INSERT INTO `tblStudent` VALUES(3, 'r0382075', 'test3', 'r0382075@student.thomasmore.be', 'Sarah', 'Pelgrims');
-INSERT INTO `tblStudent` VALUES(4, 'r0417768', 'test4', 'r0417768@student.thomasmore.be', 'Ziggy', 'Verstrepen');
+INSERT INTO `tblstudent` (`studentID`, `studentRnummer`, `studentPaswoord`, `studentMail`, `studentVoornaam`, `studentNaam`) VALUES
+(1, 'r0330949', 'test1', 'r0330949@student.thomasmore.be', 'Liesbeth', 'Vanaerschot'),
+(2, 'r0376986', 'test2', 'r0376986@student.thomasmore.be', 'Lissa', 'Sleeckx'),
+(3, 'r0382075', 'test3', 'r0382075@student.thomasmore.be', 'Sarah', 'Pelgrims'),
+(4, 'r0417768', 'test4', 'r0417768@student.thomasmore.be', 'Ziggy', 'Verstrepen');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblStudentLes`
+-- Tabelstructuur voor tabel `tblstudentles`
 --
 
-CREATE TABLE `tblStudentLes` (
+CREATE TABLE IF NOT EXISTS `tblstudentles` (
   `studentlesID` int(11) NOT NULL AUTO_INCREMENT,
   `studentID` int(11) NOT NULL,
   `lesID` int(11) NOT NULL,
@@ -176,49 +180,54 @@ CREATE TABLE `tblStudentLes` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=111 ;
 
 --
--- Gegevens worden uitgevoerd voor tabel `tblStudentLes`
+-- Gegevens worden uitgevoerd voor tabel `tblstudentles`
 --
 
-INSERT INTO `tblStudentLes` VALUES(110, 4, 19);
-INSERT INTO `tblStudentLes` VALUES(109, 4, 15);
-INSERT INTO `tblStudentLes` VALUES(108, 4, 13);
-INSERT INTO `tblStudentLes` VALUES(107, 4, 12);
-INSERT INTO `tblStudentLes` VALUES(106, 4, 8);
-INSERT INTO `tblStudentLes` VALUES(105, 4, 5);
-INSERT INTO `tblStudentLes` VALUES(104, 4, 4);
-INSERT INTO `tblStudentLes` VALUES(103, 4, 3);
-INSERT INTO `tblStudentLes` VALUES(102, 4, 2);
-INSERT INTO `tblStudentLes` VALUES(101, 3, 18);
-INSERT INTO `tblStudentLes` VALUES(100, 3, 20);
-INSERT INTO `tblStudentLes` VALUES(99, 3, 21);
-INSERT INTO `tblStudentLes` VALUES(98, 3, 22);
-INSERT INTO `tblStudentLes` VALUES(97, 3, 15);
-INSERT INTO `tblStudentLes` VALUES(96, 3, 13);
-INSERT INTO `tblStudentLes` VALUES(95, 3, 11);
-INSERT INTO `tblStudentLes` VALUES(94, 3, 9);
-INSERT INTO `tblStudentLes` VALUES(93, 3, 6);
-INSERT INTO `tblStudentLes` VALUES(92, 3, 5);
-INSERT INTO `tblStudentLes` VALUES(91, 3, 4);
-INSERT INTO `tblStudentLes` VALUES(90, 3, 2);
-INSERT INTO `tblStudentLes` VALUES(89, 3, 1);
-INSERT INTO `tblStudentLes` VALUES(88, 2, 18);
-INSERT INTO `tblStudentLes` VALUES(87, 2, 20);
-INSERT INTO `tblStudentLes` VALUES(86, 2, 21);
-INSERT INTO `tblStudentLes` VALUES(85, 2, 22);
-INSERT INTO `tblStudentLes` VALUES(84, 2, 17);
-INSERT INTO `tblStudentLes` VALUES(83, 2, 15);
-INSERT INTO `tblStudentLes` VALUES(82, 2, 13);
-INSERT INTO `tblStudentLes` VALUES(81, 2, 11);
-INSERT INTO `tblStudentLes` VALUES(80, 2, 9);
-INSERT INTO `tblStudentLes` VALUES(79, 2, 8);
-INSERT INTO `tblStudentLes` VALUES(78, 2, 6);
-INSERT INTO `tblStudentLes` VALUES(77, 2, 5);
-INSERT INTO `tblStudentLes` VALUES(76, 2, 4);
-INSERT INTO `tblStudentLes` VALUES(75, 2, 2);
-INSERT INTO `tblStudentLes` VALUES(74, 2, 1);
-INSERT INTO `tblStudentLes` VALUES(73, 1, 19);
-INSERT INTO `tblStudentLes` VALUES(72, 1, 18);
-INSERT INTO `tblStudentLes` VALUES(71, 1, 13);
-INSERT INTO `tblStudentLes` VALUES(70, 1, 12);
-INSERT INTO `tblStudentLes` VALUES(69, 1, 6);
-INSERT INTO `tblStudentLes` VALUES(68, 1, 4);
+INSERT INTO `tblstudentles` (`studentlesID`, `studentID`, `lesID`) VALUES
+(110, 4, 19),
+(109, 4, 15),
+(108, 4, 13),
+(107, 4, 12),
+(106, 4, 8),
+(105, 4, 5),
+(104, 4, 4),
+(103, 4, 3),
+(102, 4, 2),
+(101, 3, 18),
+(100, 3, 20),
+(99, 3, 21),
+(98, 3, 22),
+(97, 3, 15),
+(96, 3, 13),
+(95, 3, 11),
+(94, 3, 9),
+(93, 3, 6),
+(92, 3, 5),
+(91, 3, 4),
+(90, 3, 2),
+(89, 3, 1),
+(88, 2, 18),
+(87, 2, 20),
+(86, 2, 21),
+(85, 2, 22),
+(84, 2, 17),
+(83, 2, 15),
+(82, 2, 13),
+(81, 2, 11),
+(80, 2, 9),
+(79, 2, 8),
+(78, 2, 6),
+(77, 2, 5),
+(76, 2, 4),
+(75, 2, 2),
+(74, 2, 1),
+(73, 1, 19),
+(72, 1, 18),
+(71, 1, 13),
+(70, 1, 12),
+(69, 1, 6),
+(68, 1, 4);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
